743c6a8df7548521aeb4ec76a933291080af7a5c
Saurabh <saurabhkushwaha2030@gmail.com>
2024-02-14 22:07:35 +0530

    change the url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-g0x3.onrender.com", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
