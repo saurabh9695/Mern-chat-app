3c6418fe9fcccd73165630df1505c7bcc2f89a88
Saurabh <saurabhkushwaha2030@gmail.com>
2024-02-14 21:55:15 +0530

    update url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-g0x3.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
